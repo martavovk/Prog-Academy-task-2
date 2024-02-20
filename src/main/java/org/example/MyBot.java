6252b6fe6e00f19aad08b4d2d5770c0d9725eb19
oleg-laizik <oleglaizik@gmail.com>
2024-02-20 21:54:13 +0100

    Pictures

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -3,6 +3,8 @@ package org.example;
 import net.thauvin.erik.crypto.CryptoPrice;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class MyBot extends TelegramLongPollingBot {
@@ -16,45 +18,57 @@ public class MyBot extends TelegramLongPollingBot {
         var text = update.getMessage().getText();
 
         try {
-            var message = new SendMessage();
-            message.setChatId(chatId);
-
             if (text.equals("/start")) {
-                message.setText("Hello");
-
+                sendMessage(chatId,"Hello");
             } else if (text.equals("btc")) {
-                var price = CryptoPrice.spotPrice("BTC");
-                message.setText("BTC price:" + price.getAmount().doubleValue());
+                sendPicture(chatId,"Bitcoin.png");
+                sendPrice(chatId,"BTC");
+
+
             } else if (text.equals("eth")) {
-                var price = CryptoPrice.spotPrice("ETH");
-                message.setText("ETH price:" + price.getAmount().doubleValue());
+                sendPicture(chatId,"Eth.png");
+                sendPrice(chatId,"ETH");
             } else if (text.equals("bch")) {
-                var price = CryptoPrice.spotPrice("BCH");
-                message.setText("BCH price:" + price.getAmount().doubleValue());
+                sendPicture(chatId,"Bch.png");
+                sendPrice(chatId,"BCH");
             } else if (text.equals("all")) {
-                var price1 = CryptoPrice.spotPrice("BTC");
-                var price2=CryptoPrice.spotPrice("ETH");
-                var price3=CryptoPrice.spotPrice("BCH");
-                message.setText("BTC price:" + price1.getAmount().doubleValue() + "\n" + "ETH price:" + price2.getAmount().doubleValue() + "\n" + "BCH price:" + price3.getAmount().doubleValue());
+                sendPrice(chatId,"BCH");
+                sendPrice(chatId,"ETH");
+
             } else if (text.equals("btc,eth")) {
                 var price1 = CryptoPrice.spotPrice("BTC");
                 var price2 = CryptoPrice.spotPrice("ETH");
-                message.setText("BTC price:" + price1.getAmount().doubleValue() + "," + "ETH price:" + price2.getAmount().doubleValue());
+                sendMessage(chatId,"BTC price:" + price1.getAmount().doubleValue() + "," + "ETH price:" + price2.getAmount().doubleValue());
             } else if (text.equals("btc,eth,doge")) {
                 var price1 = CryptoPrice.spotPrice("BTC");
                 var price2 = CryptoPrice.spotPrice("ETH");
                 var price4=CryptoPrice.spotPrice("DOGE");
-                message.setText("BTC price:" + price1.getAmount().doubleValue() + "," + "ETH price:" + price2.getAmount().doubleValue() + "," + "DOGE price:" + price4.getAmount().doubleValue());
+                sendMessage(chatId,"BTC price:" + price1.getAmount().doubleValue() + "," + "ETH price:" + price2.getAmount().doubleValue() + "," + "DOGE price:" + price4.getAmount().doubleValue());
 
-        } else {message.setText("Unknown command!");
+        } else {sendMessage(chatId,"Unknown command!");
         }
 
-            execute(message);
         } catch (Exception e) {
             System.out.println("Error!");
         }
     }
-
+    void sendPrice(long chatId,String name) throws Exception{
+        var price = CryptoPrice.spotPrice("name");
+        sendMessage(chatId,name+"price:" + price.getAmount().doubleValue());
+    }
+    void sendPicture(long chatId,String name) throws Exception{
+        var photo=getClass().getClassLoader().getResourceAsStream(name);
+        var message=new SendPhoto();
+        message.setChatId(chatId);
+        message.setPhoto(new InputFile(photo,name));
+        execute(message);
+    }
+    void sendMessage(long chatId,String text) throws Exception{
+        var message=new SendMessage();
+        message.setChatId(chatId);
+        message.setText(text);
+        execute(message);
+}
 
    public String getBotUsername (){
        return "mysuperbot885_bot";
